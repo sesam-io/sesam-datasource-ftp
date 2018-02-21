bafc6a3aba43d703f5bc9e9f1315795b4c49deba
Feng Luan <feng.luan@sesam.no>
2018-02-21 21:52:30 +0100

    Supporting FTPS

--- service/proxy-service.py
+++ service/proxy-service.py
@@ -5,30 +5,66 @@ import logging
 import os
 from io import StringIO, BytesIO
 from ftplib import FTP
+from ftplib import FTP_TLS
+import ssl
 
 app = Flask(__name__)
 
+class MyFTP_TLS(FTP_TLS):
+    """Explicit FTPS, with shared TLS session"""
+    def ntransfercmd(self, cmd, rest=None):
+        conn, size = FTP.ntransfercmd(self, cmd, rest)
+        if self._prot_p:
+            session = self.sock.session
+            if isinstance(self.sock, ssl.SSLSocket):
+                session = self.sock.session
+            conn = self.context.wrap_socket(conn,
+                                            server_hostname=self.host,
+                                            session=session)  # this is the fix
+        return conn, size
+
+
 class FTPClient():
-    """docstring for FTP"""
+    """FTP Client"""
     def __init__(self, user, pwd, ftp_url):
         logger.debug("ftp connecting to {} with {}:{}".format(ftp_url, user, pwd))
         try:
-            self.ftp = FTP(ftp_url)
-            self.ftp.login(user, pwd)
+            self.client = FTP(ftp_url)
+            self.client.login(user, pwd)
         except Exception as e:
             raise e
+
+    def test(self):
+        return self.ftps.retrlines('LIST')
 
     def get_stream(self, fpath):
         """return a file stream"""
         r = BytesIO()
         logger.debug("fetching {}".format(fpath))
-        self.ftp.retrbinary('RETR {}'.format(fpath), r.write)
+        self.client.retrbinary('RETR {}'.format(fpath), r.write)
         return r
 
     def get_content(self, fpath):
         """return file as string"""
         resp = self.get_stream(fpath)
         return resp.getvalue()
+
+    def quit(self):
+        self.client.quit()
+
+
+class FTPSClient(FTPClient):
+    """FTPS Client"""
+    def __init__(self, user, pwd, ftp_url):
+        logger.debug("ftps connecting to {} with {}:{}".format(ftp_url, user, pwd))
+        try:
+            self.client = MyFTP_TLS(ftp_url)
+            self.client.login(user, pwd)
+            self.client.prot_p()
+            self.client.set_pasv(True)
+        except Exception as e:
+            raise e
+
 
 def get_var(var):
     envvar = None
@@ -67,18 +103,23 @@ def get_file(sys_id):
     if not sys_url:
         return abort(400, "Cannot find the endpoint url for {}".format(sys_id))
     f_stream = None
-    if sys_url.startswith('ftp://'):
-        try:
-            client = FTPClient(auth.username, auth.password, sys_url[6:])
-            f_stream = client.get_stream(fpath)
-            f_stream.seek(0)
-            return send_file(f_stream, attachment_filename="", as_attachment=True)
-        except Exception as e:
-            return abort(500, e)
-        #f_c = client.get_content(fpath)
-        #return Response(f_c)
-    else:
-        return abort(400, "Not supported protocal.")
+    client = None
+    try:
+        if sys_url.startswith('ftp://'):
+                client = FTPClient(auth.username, auth.password, sys_url[6:])
+        elif sys_url.startswith('ftps://'):
+                client = FTPSClient(auth.username, auth.password, sys_url[7:])
+        else:
+            return abort(400, "Not supported protocal.")
+        f_stream = client.get_stream(fpath)
+        f_stream.seek(0)
+        f_name = fpath.split('/')[-1]
+        print(f_name)
+        client.quit()
+        return send_file(f_stream, attachment_filename=f_name, as_attachment=True)
+    except Exception as e:
+        return abort(500, e)
+
 
 if __name__ == '__main__':
     # Set up logging
